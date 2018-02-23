d45a1212a3e4730ef6dbc1b2a949f1a42231ac21
Joe <jtillotson@student.neumont.edu>
2018-02-23 09:23:08 -0700

    Math implementation is done??

--- src/models/Math.java
+++ src/models/Math.java
@@ -1,4 +1,54 @@
 package models;
 
-public class Math {
+abstract public class Math {
+
+    private int x;
+
+    private int y;
+
+    private int a;
+
+    private int b;
+
+    private int c;
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
 }
