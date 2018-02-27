97a605648ffea5cf2d59c2b2c275ffd52c72afb8
Setsuna <thedarkness435@gmail.com>
2018-02-27 11:40:08 -0700

    Calculator gui layout in progress, Hamburger animation complete

---
+++ src/AnimationAssets.java
@@ -0,0 +1,50 @@
+import com.jfoenix.controls.JFXHamburger;
+
+import com.jfoenix.transitions.hamburger.HamburgerBasicCloseTransition;
+import com.jfoenix.transitions.hamburger.HamburgerSlideCloseTransition;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AnimationAssets  extends Application implements Initializable {
+    public static void main(String[] args) {
+        Application.launch(AnimationAssets.class,args);
+    }
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(this.getClass().getResource("/Resources/sample.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Material Calculator");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+    }
+
+    @FXML
+    private JFXHamburger ham;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        HamburgerSlideCloseTransition transition = new HamburgerSlideCloseTransition(ham);
+        transition.setRate(-1);
+        ham.addEventHandler(MouseEvent.MOUSE_CLICKED, (e)->{
+            transition.setRate(transition.getRate()*-1);
+            transition.play();
+        });
+    }
+
+
+
+
+
+
+}
+
--- src/models/Physics.java
+++ src/models/Physics.java
@@ -1,4 +1,35 @@
 package models;
 
-public class Physics {
+public class Physics extends MyMath {
+
+public double velocity(double changeX, double changeY){
+
+    return changeX;
+}
+
+
+public double acceleration(double changeV, double changeT){
+
+    return changeV;
+}
+
+
+public double force(double m1, double m2){
+
+    return m1;
+}
+
+
+public double speed(double velocity, double time1, double time2, double acceleration){
+
+    return velocity;
+}
+
+
+public double work(double force, double speed, double theta){
+
+    return force;
+}
+
+
 }
