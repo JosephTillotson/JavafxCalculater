263b86b12753b3ca1df55e00861bb7aa01f3b928
Joe <jtillotson@student.neumont.edu>
2018-03-06 18:30:52 -0700

    JAVA DOCS ARE DONE BOIIIIIIIIIIIIIIIIII

--- src/models/Physics.java
+++ src/models/Physics.java
@@ -3,7 +3,15 @@ package models;
 public class Physics extends MyMath {
     BasicCalculater bc=new BasicCalculater();
 
-public double velocity(double x1, double x2,double t1, double t2){
+    /**
+     * this is the velocity of an object
+     * @param x1 the first distance
+     * @param x2 second distance
+     * @param t1 first time
+     * @param t2 second time
+     * @return the velocity of the item
+     */
+    public double velocity(double x1, double x2,double t1, double t2){
     double changeX;
     double changeT;
     changeT=t1-t2;
@@ -11,8 +19,15 @@ public double velocity(double x1, double x2,double t1, double t2){
     return changeX/changeT;
 }
 
-
-public double acceleration(double v1,double v2, double t1, double t2){
+    /**
+     * Will solve for the acceleration of the object
+     * @param v1 the velocity of the object
+     * @param v2 the second velocity of the oblject
+     * @param t1 the first time
+     * @param t2 second time
+     * @return the acceleration of the object
+     */
+    public double acceleration(double v1,double v2, double t1, double t2){
     double changeV;
     double changeT;
     changeT=t1-t2;
@@ -20,45 +35,98 @@ public double acceleration(double v1,double v2, double t1, double t2){
     return changeV/changeT;
 }
 
-
-public double force(double m1, double m2){
+    /**
+     * will solve for the force of the object
+     * @param m1 the first mass
+     * @param m2 and the mass 2
+     * @return will return teh force appied
+     */
+    public double force(double m1, double m2){
     double G=6.672*bc.exponents(10,-8);
     double r2=15673681;
 
     return (G*m1*m2)/r2;
 }
 
-public double work(double force,  double distance){
+    /**
+     * will solve for the work
+     * @param force the force of the object
+     * @param distance the distance the object has gone
+     * @return the value of work
+     */
+    public double work(double force,  double distance){
 
     return force*distance;
 }
 
-public double ef(double ei, double work){
+    /**
+     * will solve for energy final
+     * @param ei energy initial
+     * @param work the work
+     * @return the energy final
+     */
+    public double ef(double ei, double work){
     return ei+work;
 }
 
-public double KE(double mass, double velocity){
+    /**
+     * will solve kenetic energy
+     * @param mass mass of the object
+     * @param velocity velocity of the object
+     * @return the value of Kenetic energy
+     */
+    public double KE(double mass, double velocity){
     return .5*mass*velocity;
 }
 
-public double PEG(double mass,double height){
+    /**
+     * will solve for the potential energy due to gravity
+     * @param mass the mass of the object
+     * @param height how heigh the object is from the ground
+     * @return the value of lthe potential energy
+     */
+    public double PEG(double mass,double height){
     double g=6.67*bc.exponents(10,-11);
     return mass*g*height;
 }
 
-public double pf(double Pi, double inpulse){
+    /**
+     * will solve for the pf of the object
+     * @param Pi p initial
+     * @param inpulse the impulse
+     * @return the value of f
+     */
+    public double pf(double Pi, double inpulse){
     return Pi+inpulse;
 }
 
-public double P(double mass, double Veloccity){
+    /**
+     * will solve for p
+     * @param mass mass of the object
+     * @param Veloccity velocity of the object
+     * @return the value of p
+     */
+    public double P(double mass, double Veloccity){
     return mass*Veloccity;
 }
 
-public double impulse(double force, double time){
+    /**
+     * will solve for impulse
+     * @param force teh force of the object
+     * @param time the time of the object
+     * @return the value for impulse
+     */
+    public double impulse(double force, double time){
     return force*time;
 }
 
-public double power(double work, double time){
+    /**
+     * will solve for the power
+     * @param work the work of te object
+     * @param time the time it is worked
+     * @return the value of power
+     */
+    public double power(double work, double time){
     return work/time;
 }
 
--- src/models/Schientific.java
+++ src/models/Schientific.java
@@ -4,68 +4,75 @@ import static java.lang.Math.*;
 import static java.lang.StrictMath.cos;
 
 public class Schientific extends MyMath{
-
+    /**
+     * Basic constructer
+     */
     public Schientific(){}
 
-    public Schientific(double x){
-        super(x);
-    }
-
-    public Schientific(double x, double y){
-        super(x,y);
-    }
-
-    public double squareroot(){
-        return sqrt(getX());
-    }
-
+    /**
+     * Will square root any function
+     * @param x the number to be rooted
+     * @return Will return the answer
+     */
     public double squareroot(double x){
         return sqrt(x);
     }
 
-    public double mySin(){
-        return sin(getX());
-    }
 
+    /**
+     * This will take the sin of the answer
+     * @param x the number to be sined?
+     * @return returnt he sin of the value
+     */
     public double mySin(double x){
         return sin(x);
     }
 
-    public double myCos(){
-        return cos(getX());
-    }
+    /**
+     * Will do the cos of a number
+     * @param x The valued to be cosined?
+     * @return the value of the number cosed
+     */
 
     public double myCos(double x){
         return cos(x);
     }
 
-    public double myTan(){
-        return tan(getX());
-    }
 
+    /**
+     * will to the tan of the number
+     * @param x the number to be taned
+     * @return return the value
+     */
     public double myTan(double x){
         return tan(x);
     }
 
-    public double mySec(){
-        return 1/sin(getX());
-    }
+    /**
+     * will do the secant of the value
+     * @param x will be the number
+     * @return return the value
+     */
 
     public double mySec(double x){
         return 1/sin(x);
     }
 
-    public double myCot(){
-        return 1/tan(getX());
-    }
+    /**
+     * Will do the cot of a value
+     * @param x number to be cot
+     * @return return the value
+     */
 
     public double myCot(double x){
         return 1/tan(x);
     }
 
-    public double mycsc(){
-        return 1/cos(getX());
-    }
+    /**
+     * will to the cosecant of the number
+     * @param x the number
+     * @return return the value
+     */
 
     public double mycsc(double x){
         return 1/cos(x);
